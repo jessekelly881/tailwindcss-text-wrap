ca12cdfea2de7997fb992e790b16d789a7e388a6
Jesse Kelly <kellyjesse881@gmail.com>
2023-10-10 01:51:13 +0200

    feat: added plugin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
-/**
- * @since 1.0.0
- */
-export const index = 1
+import plugin from 'tailwindcss/plugin';
+
+export default plugin(
+    function({ matchUtilities, theme }) { 
+        matchUtilities({
+            "text": (value) => ({
+                textWrap: value
+            }),
+        }, 
+        { values: theme('textWrap') })
+    }, 
+    {
+        theme: {
+            textWrap: {
+                "nowrap": "nowrap",
+                "wrap": 'wrap',
+                "balance": 'balance',
+                "stable": "stable",
+                "pretty": "pretty"
+            },
+        }
+    }
+) 
